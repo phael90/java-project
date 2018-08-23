4a4038f5af2b021a33a80cd35922722990d4864e
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-08-23 10:17:59 +0100

    user class created and category enum created

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -1,4 +1,23 @@
 package models;
 
 public enum Category {
+
+    VEHICLES("Vehicles"),
+    ELECTRONICS("Electronics"),
+    HOUSEHOLDITEM("Household Items"),
+    CLOTHES("Clothes"),
+    SERVICES("Services"),
+    PROPERTY("Property"),
+    ANIMALS("Animals"),
+    MISCELLANEOUS("Miscellaneous");
+
+    private String niceName;
+
+    Category(String niceName) {
+        this.niceName = niceName;
+    }
+
+    public String getNiceName() {
+        return niceName;
+    }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,73 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String eMail;
+    private List<Advert> adverts;
+
+    public User() {
+    }
+
+    public User(String username, String firstName, String lastName, String eMail) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eMail = eMail;
+        this.adverts = new ArrayList<Advert>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public List<Advert> getAdverts() {
+        return adverts;
+    }
+
+    public void setAdverts(List<Advert> adverts) {
+        this.adverts = adverts;
+    }
 }
