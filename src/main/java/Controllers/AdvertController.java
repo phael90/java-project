ea9e84ca5995f012671008762443577ff5faff9d
Raphael Ugha <raphaelugha@gmail.com>
2018-08-23 12:03:05 +0100

    Added get index in AdvertController

--- src/main/java/Controllers/AdvertController.java
+++ src/main/java/Controllers/AdvertController.java
@@ -1,4 +1,33 @@
 package Controllers;
 
+import db.DBHelper;
+import models.Advert;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class AdvertController {
+
+    public AdvertController(){
+        setUpRoutes();
+    }
+
+    public static void setUpRoutes() {
+
+        //TODO: INDEX
+        get("/adverts", (req, res) ->{
+            List<Advert> allAdverts = DBHelper.getAll(Advert.class);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("allAdverts", allAdverts);
+            model.put("template", "templates/adverts/index.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+    }
+
 }
