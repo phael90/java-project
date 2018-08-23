8c756d55e6a738837d4e1dffe198e44ad5a92d52
Raphael Ugha <raphaelugha@gmail.com>
2018-08-23 14:38:40 +0100

    Created Creaate post function in AdvertController

--- src/main/java/Controllers/AdvertController.java
+++ src/main/java/Controllers/AdvertController.java
@@ -3,6 +3,7 @@ package Controllers;
 import db.DBHelper;
 import models.Advert;
 import models.Category;
+import models.User;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import java.util.HashMap;
@@ -10,6 +11,7 @@ import java.util.List;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.Spark.redirect;
 
 public class AdvertController {
 
@@ -32,16 +34,35 @@ public class AdvertController {
         //TODO: NEW
 
         get("/adverts/new", (req, res) -> {
-
+            List<User> allUsers = DBHelper.getAll(User.class);
             Category[] allCategories = Category.values();
 
             HashMap<String, Object> model = new HashMap<>();
             model.put("allCategories", allCategories);
             model.put("template", "templates/adverts/new.vtl");
+            model.put("allUsers", allUsers);
 
             return new ModelAndView(model, "templates/layout.vtl");
 
         }, new VelocityTemplateEngine());
+
+        //CREATE
+
+        post("/adverts", (req, res) ->{
+            int user_id = Integer.parseInt(req.queryParams("user"));
+
+            User user = DBHelper.findById(User.class, user_id);
+            String title = req.queryParams("title");
+            String description = req.queryParams("desctription");
+            Category category = Category.valueOf(req.queryParams("category"));
+            double price = Double.parseDouble(req.queryParams("price"));
+
+            Advert newAdvert = new Advert(title, description, category, price, user);
+            DBHelper.save(newAdvert);
+            res.redirect("/adverts");
+            return null;
+        });
+
     }
 
 }
