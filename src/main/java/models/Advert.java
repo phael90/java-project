9859135b7dc775e831ff0c59af072762e08cf0d6
Raphael Ugha <raphaelugha@gmail.com>
2018-08-23 10:15:28 +0100

    Created Advert class

--- src/main/java/models/Advert.java
+++ src/main/java/models/Advert.java
@@ -1,4 +1,58 @@
 package models;
 
 public class Advert {
+    private int id;
+    private String title;
+    private String description;
+    private Category category;
+    private double price;
+
+    public Advert(String title, String description, Category category, double price){
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Advert(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
